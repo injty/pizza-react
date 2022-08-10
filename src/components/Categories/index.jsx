205f48bdf6bcf367a83049bcd9bbbc9670f9c083
nijat ibrahimov <nijatibrahimov@gmail.com>
2022-08-11 00:22:57 +0400

    add: category selected methods.

---
+++ src/components/Categories/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export function Categories({ value, onChangeCategory }) {
+	const categories = [
+		'Все',
+		'Мясные',
+		'Вегетарианская',
+		'Гриль',
+		'Острые',
+		'Закрытые',
+	]
+
+	return (
+		<div className="categories">
+			<ul>
+				{
+					categories.map((category, index) =>
+						<li
+							key={index}
+							className={
+								value === index ? "active" : ""
+							}
+							onClick={() => onChangeCategory(index)}
+						>
+							{category}
+						</li>
+					)
+				}
+			</ul>
+		</div>
+	)
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,43 +7,48 @@ import PizzaBlockSkeleton from '../components/PizzaBlock/PizzaBlockSkeleton'
 
 
 export default function Home() {
+	const [items, setItems] = useState([])
+	const [isLoading, setIsLoading] = useState(true)
 
-  const [items, setItems] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
+	// category
+	const [categoryIndex, setCategoryIndex] = React.useState(0)
 
-  useEffect(() => {
-    fetch("https://62ea2bcaad295463258626d6.mockapi.io/pizzas")
-      .then((res) => res.json())
-      .then((data) => {
-        setItems(data);
-        setIsLoading(false)
-      });
-    window.scrollTo(0, 0);
-  }, [])
+	useEffect(() => {
+		setIsLoading(true);
 
-  return (
-    <div className='container'>
-      <div className="content__top">
-        <Categories />
-        <Sort />
-      </div>
-      <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">
-        {
-          isLoading
-            ? [...new Array(6)].map((_, index) =>
-              <PizzaBlockSkeleton
-                key={index}
-              />
-            )
-            : items.map(obj =>
-              <PizzaBlock
-                key={obj.id}
-                {...obj}
-              />
-            )
-        }
-      </div>
-    </div>
-  )
+		const category = categoryIndex > 0 ? `category=${categoryIndex}&order=desc` : '';
+
+		fetch(`https://62ea2bcaad295463258626d6.mockapi.io/pizzas?${category}`)
+			.then((res) => res.json())
+			.then((data) => {
+				setItems(data);
+				setIsLoading(false)
+			});
+		window.scrollTo(0, 0);
+	}, [categoryIndex]);
+
+	return (
+		<div className='container'>
+			<div className="content__top">
+				<Categories value={categoryIndex} onChangeCategory={(index) => setCategoryIndex(index)} />
+				<Sort />
+			</div>
+			<h2 className="content__title">Все пиццы</h2>
+			<div className="content__items">
+				{
+					isLoading
+						? [...new Array(6)].map((_, index) => <PizzaBlockSkeleton
+							key={index}
+						/>
+						)
+						: items.map(obj =>
+							<PizzaBlock
+								key={obj.id}
+								{...obj}
+							/>
+						)
+				}
+			</div>
+		</div>
+	)
 }
